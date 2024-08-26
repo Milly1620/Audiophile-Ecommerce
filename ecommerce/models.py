79247d30d2eaaf14748ada7f101a8039953ba8b1
Millicent-Konadu <millicent.opoku@amalitech.com>
2024-08-26 11:21:34 +0000

    changes

--- ecommerce/models.py
+++ ecommerce/models.py
@@ -6,14 +6,13 @@ from django.utils.text import slugify
 
 class Product(models.Model):
     product_name = models.CharField(max_length=100)
-    slug = models.SlugField(unique= True, blank=True)
     category = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    categoryImage = models.ImageField(upload_to ='product -images/')
+    categoryImage = models.ImageField(upload_to ='category -images/')
     new = models.BooleanField(default=True)
     features = models.TextField()
-    includes = models.JSONField(default=list)
+    includes = models.TextField(default=list)
 
     def __str__(self):
          return self.name
@@ -24,9 +23,6 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
-
-
-from django.db import models
 
 
 
